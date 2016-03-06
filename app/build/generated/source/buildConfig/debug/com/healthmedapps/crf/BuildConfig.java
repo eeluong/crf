d430d7f0b0360caed863fd63ea9e6d6de0f910f5
Eric Luong <eeluong@gmail.com>
2016-03-06 11:00:37 -0600

    convert to android studio
    Signed-off-by: Eric Luong <eeluong@gmail.com>

---
+++ app/build/generated/source/buildConfig/debug/com/healthmedapps/crf/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.healthmedapps.crf;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.healthmedapps.crf";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
